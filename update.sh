02d67d7f5ef71d746b76f505e42c587f21bae9f0
dk040106 <dk040106@naver.com>
2022-02-08 22:03:46 +0900

    update update.sh for shoter code

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -cp /scripts/food/foodAPI.jar Food.java
-java -cp /scripts/food:/scripts/food/foodAPI.jar Food | python3 /scripts/food/makeJSON.py
+cd /scripts/food
+java -cp .:./foodAPI.jar Food | python3 ./makeJSON.py
